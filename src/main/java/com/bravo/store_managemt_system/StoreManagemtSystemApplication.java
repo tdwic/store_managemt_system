20f3cfb71022e85dd7835e2197c743cd5dde2d89
tdwic <wickramanayakatharindu@gmail.com>
2020-04-12 12:30:27 +0530

    initial project details

---
+++ src/main/java/com/bravo/store_managemt_system/StoreManagemtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bravo.store_managemt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagemtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagemtSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bravo/store_managemt_system/StoreManagemtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravo.store_managemt_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagemtSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
