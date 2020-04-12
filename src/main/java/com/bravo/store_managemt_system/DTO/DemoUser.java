57089ca0313aa8659422e3ad7fc8cf780c83cfc4
tdwic <wickramanayakatharindu@gmail.com>
2020-04-12 12:47:27 +0530

    ui done

---
+++ src/main/java/com/bravo/store_managemt_system/DTO/DemoUser.java
@@ -0,0 +1,32 @@
+package com.bravo.store_managemt_system.DTO;
+
+public class DemoUser {
+
+    private String name;
+    private int age;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/bravo/store_managemt_system/controler/DemoController.java
@@ -0,0 +1,33 @@
+package com.bravo.store_managemt_system.controler;
+
+import com.bravo.store_managemt_system.DTO.DemoUser;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class DemoController {
+
+    //Accept only int value and return them after process
+    @RequestMapping(value = "/demo/{j}", method = RequestMethod.GET)
+    public int showData(@PathVariable int j){
+        System.out.println("this is the value of " + j);
+        j=j+23;
+
+        return j;
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public DemoUser userRegistration(@RequestBody DemoUser demoUser){
+        System.out.println("Name " + demoUser.getName());
+        System.out.println("Age "+demoUser.getAge());
+        System.out.println("Role " + demoUser.getRole());
+
+        return demoUser;
+
+    }
+
+
+
+}
