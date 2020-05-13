e519ab98ccdc846489fc6e355885dbfefdc16460
Ovi98 <53076284+Ovi98@users.noreply.github.com>
2020-05-13 23:40:19 +0530

    Login and Signup UI

--- src/main/store_management_ui/src/Components/Authenticaton/SignUp.js
+++ src/main/store_management_ui/src/Components/Authenticaton/SignUp.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class SignUp extends Component {
   render() {
+
+    const myStyle = {
+       width: "400px",
+    };
+
     return (
-        <h1>SignUp page is loading</h1>
+        <>
+        <h1>CREATE ACCOUNT</h1>
+        <br/>
+
+        <div class="d-flex justify-content-center">
+        <Form style={myStyle}>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text" placeholder="First Name" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Last Name" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="text" placeholder="Email Address" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        </Form>
+        </div>
+
+        <br/>
+        <Button variant="primary" type="submit">
+           CREATE ACCOUNT
+        </Button>
+        </>
    
     );
   }
