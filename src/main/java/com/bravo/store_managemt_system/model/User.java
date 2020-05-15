51e99596d56aed385c187ab2c215be41f31eb7ac
Ovi98 <53076284+Ovi98@users.noreply.github.com>
2020-05-15 14:38:37 +0530

    User Model

---
+++ src/main/java/com/bravo/store_managemt_system/model/User.java
@@ -0,0 +1,75 @@
+package com.bravo.store_managemt_system.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private int id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private boolean status;
+    private int role;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
