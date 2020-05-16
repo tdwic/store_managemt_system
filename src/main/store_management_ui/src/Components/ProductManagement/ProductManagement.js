98ee1af0f607833901f60e24355aa52f73443851
tdwic <36078972+tdwic@users.noreply.github.com>
2020-05-16 21:45:57 +0530

    routing completed,
    product manager fore done

--- src/main/store_management_ui/src/Components/ProductManagement/ProductManagement.js
+++ src/main/store_management_ui/src/Components/ProductManagement/ProductManagement.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import './ProductManagement.css'
 
 class ProductManagement extends Component {
     constructor(props) {
@@ -7,8 +10,63 @@ class ProductManagement extends Component {
     }
     render() {
         return (
-            <div>
-<h3>hii</h3>
+            <div className="mainDiv">
+                <Form className="mainForm">
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="productName">
+                            <Form.Label>Product Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Product Name" />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+
+                        <Form.Group as={Col}>
+                            <Form.Label>Product Price</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Product Price" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} >
+                            <Form.Label>Product Discount</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Product Discount" />
+                        </Form.Group>
+
+                    </Form.Row>
+
+                    <Form.Row>
+
+                        <Form.Group as={Col}>
+                            <Form.Label>Product Rating</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Product Rating" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} >
+                            <Form.Label>Product Category</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Product Category" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
+
+                            <Form.Label>Product Description</Form.Label>
+                          
+                                <input type="file"></input>
+                            
+                            
+                        </Form.Group>
+
+                    </Form.Row>
+
+                    <Form.Row>
+
+                        <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Product Description</Form.Label>
+                            <Form.Control className="prodDescription" as="textarea" rows="10" />
+                        </Form.Group>
+
+                    </Form.Row>
+
+                </Form>
             </div>
         );
     }
