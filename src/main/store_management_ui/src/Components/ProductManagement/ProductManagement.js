4e7c545dc767a604daee36446453fbcabc9793ac
tdwic <36078972+tdwic@users.noreply.github.com>
2020-05-17 05:41:58 +0530

    adding done,
    remove done
    font awesome installed
    table show done

--- src/main/store_management_ui/src/Components/ProductManagement/ProductManagement.js
+++ src/main/store_management_ui/src/Components/ProductManagement/ProductManagement.js
@@ -4,21 +4,41 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import './ProductManagement.css'
-import { CommonGet, CommonDeleteById } from '../../config';
+import { CommonGet, CommonDeleteById, CommonPost } from '../../config';
+
 
 class ProductManagement extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            productName:'',
+            productId:'',
+            productCategory:'',
+            prodDescription:'',
+            productDiscount:'',
+            productPrice:'',
+            productRating:'',
 
-            productName: '',
+            categoryList: [],
             productList: [] ,
             isLoaded : false
           };
     }
 
     componentDidMount(){
+
+        this.setState({
+            productName:'',
+            productId:'',
+            productCategory:'',
+            prodDescription:'',
+            productDiscount:'',
+            productPrice:'',
+            productRating:'',
+        })
+
         this.fetchProductList();
+        this.fetchCategoryList();
     }
 
     fetchProductList(){
@@ -33,16 +53,65 @@ class ProductManagement extends Component {
         });
     }
 
-    addNewProduct(product){
-        let newProduct = {
-            // productName
-            // productPrice
-            // productDiscount
-            // productImageRef
-            // productDescription
-            // productRating
-            // productCategory
+    fetchCategoryList(){
+        CommonGet('listCategoryDet','')
+        .then(res=>res.json())
+        .then(resultSet =>{
+            this.setState({
+                isLoaded:true,
+                categoryList: resultSet
+            })
+            console.log(this.state.categoryList);
+        });
+    }
+
+    addNewProduct = (event) => {
+
+        let formData={
+                "productName":this.state.productName,
+                "productPrice":this.state.productPrice,
+                "productDiscount":this.state.productDiscount,
+                "productImageRef":"this.state.pro",
+                "productDescription":this.state.prodDescription,
+                "productRating":2,
+                //"productRating":this.state.productRating,
+                "productCategory":this.state.productCategory
         }
+
+        CommonPost('product',formData)
+            .then(res=>res.json())
+            .then(json =>{
+                this.setState({
+                    isLoaded : true
+                })
+                this.componentDidMount();
+        });
+
+
+    }
+
+
+    handleChange=(event)=>{
+
+        console.log("event.target 1 "+event.target.value);
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+
+        
+
+    }
+
+    handleClick = (eve) => {
+        let val = eve.target.value;
+         this.setState({productCategory:val},() => {
+            console.log(this.state.productCategory)
+        })
+    };
+
+    
+    updateProduct(product){
+        console.log("ss"+product.name);
     }
 
     removeProductById(productId){
@@ -58,12 +127,12 @@ class ProductManagement extends Component {
         return (
             <div className="mainDiv">
                 <div>
-                    <Form className="mainForm">
+                    <Form className="mainForm" onSubmit={this.addNewProduct}>
 
                         <Form.Row>
-                            <Form.Group as={Col} controlId="productName">
+                            <Form.Group as={Col}>
                                 <Form.Label>Product Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Product Name" />
+                                <Form.Control name='productName' value={this.state.productName} onChange={this.handleChange} type="text" placeholder="Enter Product Name" />
                             </Form.Group>
                         </Form.Row>
 
@@ -71,12 +140,12 @@ class ProductManagement extends Component {
 
                             <Form.Group as={Col}>
                                 <Form.Label>Product Price</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Product Price" />
+                                <Form.Control name='productPrice' value={this.state.productPrice} onChange={this.handleChange} type="text" placeholder="Enter Product Price" />
                             </Form.Group>
 
                             <Form.Group as={Col} >
                                 <Form.Label>Product Discount</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Product Discount" />
+                                <Form.Control name='productDiscount' value={this.state.productDiscount} onChange={this.handleChange} type="text" placeholder="Enter Product Discount" />
                             </Form.Group>
 
                         </Form.Row>
@@ -85,18 +154,26 @@ class ProductManagement extends Component {
 
                             <Form.Group as={Col}>
                                 <Form.Label>Product Rating</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Product Rating" />
+                                <Form.Control name='productRating' value={this.state.productRating} onChange={this.handleChange} type="text" placeholder="Enter Product Rating" />
                             </Form.Group>
 
                             <Form.Group as={Col} >
                                 <Form.Label>Product Category</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Product Category" />
+                                {/* <Form.Control name='productCategory' value={this.state.productCategory} onChange={this.handleChange} type="text" placeholder="Enter Product Category" /> */}
+                                <Form.Control as="select" onChange={this.handleClick} custom>
+                                    {this.state.categoryList.map((category) => (
+                                        <option key={category.categoryId} value={category.categoryId}>
+                                            {category.categoryName}{category.categoryId}
+                                        </option>
+                                    ))}
+                                </Form.Control>
+                               
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
                                 
-                                <Form.Label>Product Description</Form.Label>
-                                <input type="file"></input>
+                                <Form.Label>Product Image</Form.Label>
+                                {/* <input name='productPrice' value={this.state.productPrice} onChange={this.handleChange} type="file"></input> */}
                             
                             </Form.Group>
 
@@ -106,15 +183,16 @@ class ProductManagement extends Component {
 
                             <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Product Description</Form.Label>
-                                <Form.Control className="prodDescription" as="textarea" rows="10" />
+                                <Form.Control name='prodDescription' value={this.state.prodDescription} onChange={this.handleChange} className="prodDescription" as="textarea" rows="10" />
                             </Form.Group>
 
                         </Form.Row>
 
                             <Form.Row>
                                 <Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
-                                    <Button variant="success">Add Product</Button>
+                                    <Button type="submit" variant="success">Add Product</Button>
                                 </Form.Group>
+                                
                             </Form.Row>
 
                         </Form>
@@ -144,12 +222,12 @@ class ProductManagement extends Component {
                                         <tr key={element.productId}>
                                             <td>{index + 1}</td>
                                             <td>{element.productName}</td>
-                                            <td>Rs:{element.productPrice}</td>
+                                            <td>Rs: {element.productPrice}</td>
                                             <td>{element.productDiscount}%</td>
                                             <td>{element.productDescription}</td>
                                             <td>{element.productRating}</td>
                                             <td>{element.productCategory}</td>
-                                            <td><Button variant="warning">Update</Button></td>
+                                            <td><Button variant="warning" onClick={(event) => this.updateProduct(element)}>Update</Button></td>
                                             <td><Button variant="danger" onClick={(event) => this.removeProductById(element.productId)}>Delete</Button></td>
                                         </tr>
                                     )
